a6a57534ce758e31cbbdf43dbdce74076254fa7f
Youliang.Chen <cyliang1113@126.com>
2019-10-14 13:50:42 +0800

    111

--- src/test/java/ConsumerDemo.java
+++ src/test/java/ConsumerDemo.java
@@ -1,3 +1,4 @@
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
@@ -7,40 +8,46 @@ import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.junit.Test;
 
 import java.util.List;
+import java.util.concurrent.locks.LockSupport;
 
+@Slf4j
 public class ConsumerDemo {
-    public static void main(String[] args) throws Exception {
+
+    @Test
+    public void consumerTest1() throws Exception {
 
         // Instantiate with specified consumer group name.
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("ProducerGroup_Consumer22");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("demo_consumer_group_2");
 
         // Specify name server addresses.
-        consumer.setNamesrvAddr("10.200.146.55:9876");
+        consumer.setNamesrvAddr("172.18.66.23:9876");
 
         // Subscribe one more more topics to consume.
-        consumer.subscribe("Topic_Topic11", "*");
+        consumer.subscribe("demo_topic_1", "*");
         // Register callback to execute on arrival of messages fetched from brokers.
         consumer.registerMessageListener(new MessageListenerConcurrently() {
 
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
                                                             ConsumeConcurrentlyContext context) {
-                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
-                for (MessageExt msg : msgs) {
-                    byte[] body = msg.getBody();
-                    String s = new String(body);
-                    System.out.printf(s);
+                synchronized (this) {
+                    log.info(Thread.currentThread().getName() + ", " + msgs);
+                    for (MessageExt msg : msgs) {
+                        byte[] body = msg.getBody();
+                        String s = new String(body);
+                        log.info(s);
+                    }
                 }
-
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
         });
 
         //Launch the consumer instance.
         consumer.start();
-
-        System.out.printf("Consumer Started.%n");
+        log.info("Consumer Started.");
+        LockSupport.park();
     }
 }
--- src/test/java/ProducerDemo.java
+++ src/test/java/ProducerDemo.java
@@ -1,19 +1,24 @@
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.junit.Test;
 
+@Slf4j
 public class ProducerDemo {
-    public static void main(String[] args) throws Exception {
-        DefaultMQProducer producer = new DefaultMQProducer("ProducerGroup_Prod11");
-        producer.setNamesrvAddr("10.200.146.55:9876");
-        producer.setVipChannelEnabled(false);
+
+    @Test
+    public void producerTest1() throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("demo_producer_group_1");
+        producer.setNamesrvAddr("172.18.66.23:9876");
+//        producer.setVipChannelEnabled(false);
         producer.start();
         for (int i = 0; i < 5; i++) {
-            Message msg = new Message("Topic_Topic11" ,"TagA",
-                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            Message msg = new Message("demo_topic_1" ,"Tag_1",
+                    ("Hello RocketMQ " + i + ", " + System.currentTimeMillis()).getBytes(RemotingHelper.DEFAULT_CHARSET));
             SendResult sendResult = producer.send(msg);
-            System.out.printf("%s%n", sendResult);
+            log.info(sendResult.toString());
         }
         producer.shutdown();
 
